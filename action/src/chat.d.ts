fe0cda4401d6dbb22c3e38cc834eb128a575fead
rizkytm <ritrime97m@gmail.com>
2026-02-16 15:39:42 +0700

    fix: rebuild

--- action/src/chat.d.ts
+++ action/src/chat.d.ts
@@ -1,15 +1,8 @@
 export declare class Chat {
     private anthropic;
     constructor(apikey: string);
-    private generateSystemPrompt;
-    private generateUserPrompt;
-    codeReview: (patch: string) => Promise<Array<{
+    codeReview: (patch: string) => Promise<{
         lgtm: boolean;
-        review_comment: string;
-        hunk_header?: string;
-    }> | {
-        lgtm: boolean;
-        review_comment: string;
-        hunk_header?: string;
+        review_comment?: string;
     }>;
 }
